e94a28c2b33d588f7df61e565fa5bddb3c2f898b
Felix Hultin <felixhultin@yahoo.se>
2019-10-03 14:10:45 +0200

    Fabians Code assesment

--- preprocess_corpus.py
+++ preprocess_corpus.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 class ParallelCorpus:
     """A sentence-level parallel corpus."""
 
@@ -16,14 +17,30 @@ class ParallelCorpus:
         than twice as long as the other side. Length is measured in number of
         characters.
         """
-        pass # TODO Task 1
+        self.translations[:] = [tup for tup in self.translations if not
+                                (len(tup[0]) * 2 < len(tup[1]) or len(tup[1]) * 2 < len(tup[0]))]
 
     def normalise_whitespace(self):
         """Replaces non-breaking whitespace (U+00A0) with regular whitespace
         (U+0020) in the source and target side of all translations.
         """
-        pass # TODO Task 1
+        indices = set()
+        for i, tup in enumerate(self.translations):
+            for k, sentence in enumerate(tup):
+                if u"\u00A0" in sentence:
+                    indices.add((i, k))
+
+        for index in indices:
+            source, target = self.translations[index[0]]
+            if index[1] == 0:
+                self.translations[index[0]] = (source.replace(u"\u00A0", ' '), target)
+            else:
+                self.translations[index[0]] = (source, target.replace(u"\u00A0", ' '))
 
 
 if __name__ == "__main__":
+    new_corpus = ParallelCorpus('data/corpus.de', 'data/corpus.en')
+    new_corpus.clean()
+    new_corpus.normalise_whitespace()
+
     pass # TODO Task 3
--- translate.py
+++ translate.py
@@ -2,6 +2,7 @@
 
 from argparse import ArgumentParser
 import requests
+from requests.exceptions import HTTPError
 
 
 def translate(sentence: str) -> str:
@@ -11,7 +12,21 @@ def translate(sentence: str) -> str:
     API endpoint: https://mintzberg.textshuttle.ai/api/v1
     API specification: https://mintzberg.textshuttle.ai/docs/
     """
-    pass # TODO Task 2
+    endpoint = 'https://mintzberg.textshuttle.ai/api/v1'
+    header = '{"segments": [' + sentence + '],"source": "en","target": "de","split_sentences": false}'
+
+    try:
+        response = requests.get(endpoint, header)
+
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')  # Python 3.6
+    except Exception as err:
+        print(f'Other error occurred: {err}')  # Python 3.6
+    else:
+        print(repr(response.json()))
+        print(repr(response.headers))
+
+    return ''
 
 
 if __name__ == "__main__":
